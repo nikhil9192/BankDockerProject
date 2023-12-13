835b5b39d8eb8514282abac6c98711520c8954a6
nikhil9192 <nikhilsinghtech2580@gmail.com>
2023-12-13 19:28:37 +0530

    Add project files.

---
+++ BankDockerProject/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using BankDockerProject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankDockerProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(User model)
+        {
+           
+                // Successful login, redirect to dashboard
+                return RedirectToAction("Index", "Dashboard");
+            }
+            // Invalid login, show error message
+           
+    }
+}
